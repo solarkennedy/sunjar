36c075ee0ecbebec80f37bc9768d98dc77d61f4a
Kyle Anderson <kyle@xkyle.com>
2020-03-07 16:44:22 -0800

    Wifi OTA support

--- sunjar.ino
+++ sunjar.ino
@@ -1,3 +1,13 @@
+#include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+char ssid[32] = "";
+char password[32] = "";
+
 const char compile_date[] = __DATE__ " " __TIME__;
 
 
@@ -12,9 +22,11 @@ void loop() {
 
 void setupSerial() {
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   Serial.println();
   Serial.println();
   Serial.println();
   Serial.printf("ESP8266 Chip id = %08X\n", ESP.getChipId());
   Serial.printf("Sketch compiled %s\n", compile_date);
+  wifiSetup();
 }
---
+++ wifi.ino
@@ -0,0 +1,70 @@
+void wifiSetup() {
+  loadCredentials();
+
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+  Serial.print("With password: ");
+  Serial.println(password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.setHostname("sunjar");
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loadCredentials() {
+  EEPROM.begin(512);
+  EEPROM.get(0, ssid);
+  EEPROM.get(0 + sizeof(ssid), password);
+  char ok[2 + 1];
+  EEPROM.get(0 + sizeof(ssid) + sizeof(password), ok);
+  EEPROM.end();
+  if (String(ok) != String("OK")) {
+    ssid[0] = 0;
+    password[0] = 0;
+  }
+  Serial.println("Recovered credentials: ");
+  Serial.print(ssid);
+  Serial.print(" / ");
+  Serial.println(password);
+}
