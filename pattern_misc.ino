e99fd5e5a44286e26d24018f10c72175f422095e
Kyle Anderson <kyle@xkyle.com>
2020-03-22 19:14:04 -0700

    Added plasma pattern

--- pattern_misc.ino
+++ pattern_misc.ino
@@ -1,4 +1,10 @@
 
+#define qsuba(x, b)  ((x>b)?x-b:0)
+CRGBPalette16 currentPalette;                                 // Palette definitions
+CRGBPalette16 targetPalette;
+TBlendType currentBlending = LINEARBLEND;
+
+
 void rainbow()
 {
   // FastLED's built-in rainbow generator
@@ -58,23 +64,50 @@ void juggle() {
 
 
 //code from https://gist.github.com/kriegsman/99082f66a726bdff7776
-const CRGB lightcolor(8,5,1);
+const CRGB lightcolor(8, 5, 1);
 
 void softtwinkles() {
-  for( int i = 0; i < NUM_LEDS; i++) {
-    if( !leds[i]) continue; // skip black pixels
-    if( leds[i].r & 1) { // is red odd?
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    if ( !leds[i]) continue; // skip black pixels
+    if ( leds[i].r & 1) { // is red odd?
       leds[i] -= lightcolor; // darken if red is odd
     } else {
       leds[i] += lightcolor; // brighten if red is even
     }
   }
-
   // Randomly choose a pixel, and if it's black, 'bump' it up a little.
   // Since it will now have an EVEN red component, it will start getting
   // brighter over time.
-  if( random8() < 40) {
+  if ( random8() < 40) {
     int j = random16(NUM_LEDS);
-    if( !leds[j] ) leds[j] = lightcolor;
+    if ( !leds[j] ) leds[j] = lightcolor;
   }
+}
+
+
+void plasma() {                                                 // This is the heart of this program. Sure is short. . . and fast.
+
+  int thisPhase = beatsin8(6, -64, 64);                         // Setting phase change for a couple of waves.
+  int thatPhase = beatsin8(7, -64, 64);
+
+  for (int k = 0; k < NUM_LEDS; k++) {                          // For each of the LED's in the strand, set a brightness based on a wave as follows:
+
+    int colorIndex = cubicwave8((k * 23) + thisPhase) / 2 + cos8((k * 15) + thatPhase) / 2; // Create a wave and add a phase change and add another wave with its own phase change.. Hey, you can even change the frequencies if you wish.
+    int thisBright = qsuba(colorIndex, beatsin8(7, 0, 96));                               // qsub gives it a bit of 'black' dead space by setting sets a minimum value. If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+
+    leds[k] = ColorFromPalette(currentPalette, colorIndex, thisBright, currentBlending);  // Let's now add the foreground colour.
+  }
+
+  EVERY_N_MILLISECONDS(100) {
+    uint8_t maxChanges = 24;
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);   // AWESOME palette blending capability.
+  }
+
+
+  EVERY_N_SECONDS(5) {                                 // Change the target palette to a random one every 5 seconds.
+    uint8_t baseC = random8();                         // You can use this as a baseline colour if you want similar hues in the next line.
+    targetPalette = CRGBPalette16(CHSV(baseC + random8(32), 192, random8(128, 255)), CHSV(baseC + random8(32), 255, random8(128, 255)), CHSV(baseC + random8(32), 192, random8(128, 255)), CHSV(baseC + random8(32), 255, random8(128, 255)));
+  }
+
+
 }
